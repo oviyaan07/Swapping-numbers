2975afbbd9a259a04e45453a205d3e72f4e2fbe0
oviyaan07 <oviyaansingh04@gmail.com>
2024-12-06 23:09:24 +0530

    first commit

---
+++ main.py
@@ -0,0 +1,9 @@
+x=int(input("enter the value of x" ))
+y=int(input("enter the value of y" ))
+z=int(input("enter the value of z" ))
+print(f"value of x, y and z before swapping is {x}, {y} and {z} respectively")
+temp=x
+x=y
+y=z
+z=temp
+print(f"value of x, y and z after swapping swapping is {x}, {y} and {z} respectively")
